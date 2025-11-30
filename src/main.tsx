760973e45daf2cb6ec1d3c4f6cd3c1184627bf9d
Tea Dice Developer <zuxell447@gmail.com>
2025-11-30 05:23:36 +0500

    Fix: Move App.tsx to src/ and fix imports for Vercel build

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 import { AppProvider } from './context/AppContext';
 
 const rootElement = document.getElementById('root');
