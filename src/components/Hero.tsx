61eff0ecfe0fa0e14ac097aff174893826a873ef
Tea Dice Developer <zuxell447@gmail.com>
2025-12-01 04:05:50 +0500

    Remove blue background spot

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,8 +12,7 @@ const Hero: React.FC = () => {
       {/* Background gradient effect */}
       <div className="absolute inset-0 z-0">
         <div className="absolute top-20 left-10 w-64 h-64 bg-[#9333ea]/20 rounded-full blur-3xl"></div>
-        <div className="absolute bottom-20 right-10 w-96 h-96 bg-[#3b82f6]/20 rounded-full blur-3xl"></div>
-        <div className="absolute inset-0 bg-gradient-to-br from-[#9333ea]/5 via-transparent to-[#3b82f6]/5"></div>
+        <div className="absolute inset-0 bg-gradient-to-br from-[#9333ea]/5 via-transparent to-transparent"></div>
       </div>
 
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
